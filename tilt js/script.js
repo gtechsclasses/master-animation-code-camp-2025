6b15ed98a8d354776d13add346587ca15a6eebaf
Anish kumar <anishkr2842003@gmail.com>
2025-02-14 10:52:47 +0530

    animation day 1

---
+++ tilt js/script.js
@@ -0,0 +1,19 @@
+/*
+${'.class'}
+${'#id'}
+${'tag'}
+
+
+*/
+
+$('.box2').tilt({
+    glare: true,
+    maxGlare: 10,
+    reset: true,
+    scale: 1.2, // for zoom use more than 1 and for zoom-out use less than 1
+})
+
+$('.box3').tilt({
+    glare: true,
+    maxGlare: 2,
+});
